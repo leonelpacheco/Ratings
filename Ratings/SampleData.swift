f4693d631b265449b5dcb45ee1fbf637b7d7e704
leonelpacheco <leonel.pacheco@gmail.com>
2015-06-25 11:49:06 -0500

    Inicial

---
+++ Ratings/SampleData.swift
@@ -0,0 +1,18 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by Caroline Begbie on 15/04/2015.
+//  Copyright (c) 2015 Caroline Begbie. All rights reserved.
+//
+
+import Foundation
+
+
+
+//Set up sample data
+
+let playersData = [ Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+  Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+  Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
+
